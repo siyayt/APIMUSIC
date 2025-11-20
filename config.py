57c8015dcdfcb2252723e47efa1ff1fac7e97bee
siyayt <rajayt0825@gmail.com>
2025-11-20 20:56:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,31 +12,31 @@ API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
 
 # Bot and owner info
-OWNER_USERNAME = getenv("OWNER_USERNAME", "SHIVANSHDEVS")
-BOT_USERNAME = getenv("BOT_USERNAME", "SapnaMusicRobot")
-BOT_NAME = getenv("BOT_NAME", "SAPNA")
-ASSUSERNAME = getenv("ASSUSERNAME", "KHWAAISH_HOON")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "ll_Raja_Babu_ll")
+BOT_USERNAME = getenv("BOT_USERNAME", "Nysamusicbot")
+BOT_NAME = getenv("BOT_NAME", "Nysa Music")
+ASSUSERNAME = getenv("ASSUSERNAME", "Nysa")
 
 # MongoDB
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
 # Limits and IDs
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
-LOGGER_ID = int(getenv("LOGGER_ID", -1002018556839))
-OWNER_ID = int(getenv("OWNER_ID", 6762113050))
+LOGGER_ID = int(getenv("LOGGER_ID", -1002260119965))
+OWNER_ID = int(getenv("OWNER_ID", 7391857972))
 
 # Heroku
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 # Git
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/itzshukla/APIMUSIC")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/siyayt/APIMUSIC")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 # Support
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ITSZSHUKLA")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/MASTIWITHFRIENDSXD")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/BOTXPRO1")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+aUamAA43_p4xMWE1")
 
 # YT-API Get from @AmShashank on tg
 YOUR_API_KEY = getenv("YOUR_API_KEY", "")
