9f6d8f87583451efaa7b81d64fc1ca456778a3ee
siyayt <rajayt0825@gmail.com>
2026-05-04 10:51:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,8 +39,9 @@ SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/BOTXPRO1")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+aUamAA43_p4xMWE1")
 
 # YT-API Get from @AmShashank on tg
-YOUR_API_KEY = getenv("YOUR_API_KEY", "")
-YOUR_API_URL = getenv("YOUR_API_URL", "")
+API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
+VIDEO_API_URL = getenv("VIDEO_API_URL", 'https://api.video.thequickearn.xyz')
+API_KEY = getenv("API_KEY", None)
 
 # Assistant settings
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "False")
